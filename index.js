01eb6fe4d4c206ccc460a62e6af88ac080d0adba
Gabriele Farina <gabr.farina@gmail.com>
2016-09-11 00:02:32 -0400

    Provide a working window for jquery

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 var hexo = hexo || {};
-hexo.extend.filter.register('$', require('jquery'));
+
+var jsdom = require('jsdom');
+
+jsdom.env("", function(err, window) {
+  if (err) {
+    console.error(err);
+  } else {
+    var jq = require('jquery')(window);
+    hexo.extend.helper.register('jquery', function() {
+      return jq;
+    });
+  }
+});
+
