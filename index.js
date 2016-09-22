552919f77f14e2ae681537a8f10b126ccaf91b1c
Gabriele Farina <gfarina@cs.cmu.edu>
2016-09-22 00:40:03 -0400

    Use Fibers to synchronously load jquery

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 var hexo = hexo || {};
-
+var Fiber = require('fibers')
 var jsdom = require('jsdom');
 
-jsdom.env("", function(err, window) {
-  if (err) {
-    console.error(err);
-  } else {
-    var jq = require('jquery')(window);
-    hexo.extend.helper.register('jquery', function() {
-      return jq;
-    });
-  }
-});
+var release = false;
 
+Fiber(function() {
+  var fiber = Fiber.current;
+
+  var register_jquery = function(err, window) {
+    if (err) {
+      fiber.throwInto(err);
+    } else {
+      var jq = require('jquery')(window);
+      hexo.extend.helper.register('jquery', function() {
+        return jq;
+      });
+    }
+
+    fiber.run();
+  };
+
+  hexo.log.debug('[hexo-jquery] Waiting for jquery to load...')
+  jsdom.env("", register_jquery);
+  Fiber.yield();
+  hexo.log.debug('[hexo-jquery] Jquery loaded');
+}).run();
